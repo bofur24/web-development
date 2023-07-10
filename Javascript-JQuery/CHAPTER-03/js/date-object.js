830e65fc27258d8d437fc6b3ba35bd652b528629
Keith Heffington <lightingcount_@hotmail.com>
2023-07-10 09:08:32 -0500

    Added dat-object.js back

--- Javascript-JQuery/CHAPTER-03/js/date-object.js
+++ Javascript-JQuery/CHAPTER-03/js/date-object.js
@@ -2,4 +2,4 @@ var today = new Date();
 var year = today.getFullYear();
 
 var el = document.getElementById('footer');
-el.innerHTML = '<p>Copyrigth &copy;' + year + '<p>'
+el.innerHTML = '<p>Copyright &copy;' + year + '</p>';
